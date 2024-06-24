943267a2c01115d11955c54dddc3b8cb5275d410
Ali Alipoor <alialipoordev@gmail.com>
2024-06-24 17:37:35 +0330

    add layout

---
+++ components/layout/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavUI from "../module/NavUi";
+
+function Header() {
+  return (
+    <div>
+      <NavUI cartItemsCount={0} />
+    </div>
+  );
+}
+
+export default Header;
