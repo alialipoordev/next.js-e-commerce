e217194c45dc36223f5a7b6705d2bb401d714755
Ali Alipoor <alialipoordev@gmail.com>
2024-06-25 20:06:39 +0330

    add password strength indicator and input Form to sign Up Page

--- components/template/SignUpPage.tsx
+++ components/template/SignUpPage.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import React from "react";
-import { Button, Input } from "@material-tailwind/react";
+import { Button } from "@material-tailwind/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import AuthFormContainer from "../module/AuthFormContainer";
 import { useFormik } from "formik";
 import * as yup from "yup";
+import InputForm from "../module/InputForm";
+import { CiUser } from "react-icons/ci";
+import { FiLock, FiMail } from "react-icons/fi";
+import zxcvbn from "zxcvbn";
 
 const validationSchema = yup.object().shape({
   name: yup.string().required("Name is required!"),
@@ -15,9 +19,12 @@ const validationSchema = yup.object().shape({
     .min(6, "Password must be at least 6 characters.")
     .max(52, "Password must be less than 6 characters.")
     .required("Password is required!"),
+  confirmPassword: yup.string().oneOf([yup.ref('password'), null], "Password doesn't match!")
 });
 
 function SignUpPage() {
+  const [passwordScore, setPasswordScore] = React.useState(0);
+
   const formErrors: string[] = [];
 
   const {
@@ -34,33 +41,73 @@ function SignUpPage() {
       name: "",
       email: "",
       password: "",
+      confirmPassword: "",
     },
     onSubmit: (values) => {
       console.log(values);
     },
   });
 
+  const validatePasswordStrength = () => {
+    let password = values.password;
+    return zxcvbn(password ? password : "").score;
+  };
+
+  React.useEffect(() => {
+    setPasswordScore(validatePasswordStrength());
+  }, [values.password]);
+
   return (
     <div className="h-screen flex items-center justify-center">
       <AuthFormContainer title="Create New Account" onSubmit={handleSubmit}>
-        <Input
+        <InputForm
           name="name"
           label="Name"
+          type="text"
+          icon={<CiUser />}
+          placeholder="example"
           value={values.name}
           onChange={handleChange}
+          disabled={isSubmitting}
+          error={errors?.name}
+          touched={touched?.name}
         />
-        <Input
+        <InputForm
           name="email"
           label="Email"
+          type="text"
+          icon={<FiMail />}
+          placeholder="example@example.com"
           value={values.email}
           onChange={handleChange}
+          disabled={isSubmitting}
+          error={errors?.email}
+          touched={touched?.email}
         />
-        <Input
+        <InputForm
           name="password"
           label="Password"
           type="password"
+          icon={<FiLock />}
+          placeholder="******"
           value={values.password}
           onChange={handleChange}
+          disabled={isSubmitting}
+          error={errors?.password}
+          touched={touched?.password}
+          passwordScore={passwordScore}
+        />
+        <InputForm
+          name="confirmPassword"
+          label="Confirm password"
+          type="password"
+          icon={<FiLock />}
+          placeholder="******"
+          value={values.confirmPassword}
+          onChange={handleChange}
+          error={errors?.confirmPassword}
+          touched={touched?.password}
+          disabled={isSubmitting}
         />
         <Button type="submit" className="w-full">
           Sign up
