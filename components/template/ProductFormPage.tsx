43866a0980c0a5fd727341d2d14ef18f40fb6d6c
Ali Alipoor <alialipoordev@gmail.com>
2024-07-10 15:49:23 +0330

    Removes an image from product Image Source and updates the state/UI

--- components/template/ProductFormPage.tsx
+++ components/template/ProductFormPage.tsx
@@ -22,6 +22,7 @@ import { NewProductInfo } from "@/types";
 interface ProductFormPageProps {
   initialValue?: InitialValue;
   onSubmit(values: NewProductInfo): void;
+  onImageRemove?(source: string): void;
 }
 
 export interface InitialValue {
@@ -48,11 +49,12 @@ const defaultValue = {
 };
 
 export default function ProductFormPage({
+  onImageRemove,
   onSubmit,
   initialValue,
 }: ProductFormPageProps) {
   const [isPending, startTransition] = useTransition();
-  const [images, setImages] = useState<File[]>([]);
+  const [imageFiles, setImageFiles] = useState<File[]>([]);
   const [thumbnail, setThumbnail] = useState<File>();
   const [isForUpdate, setIsForUpdate] = useState(false);
   const [productInfo, setProductInfo] = useState({ ...defaultValue });
@@ -85,8 +87,28 @@ export default function ProductFormPage({
   };
 
   const removeImage = async (index: number) => {
-    const newImages = images.filter((_, idx) => idx !== index);
-    setImages([...newImages]);
+    if (!productImagesSource) return;
+    //remove from cloud
+    const imageToRemove = productImagesSource[index];
+    const cloudSourceUrl = "https://res.cloudinary.com";
+    if (imageToRemove.startsWith(cloudSourceUrl)) {
+      onImageRemove && onImageRemove(imageToRemove);
+    } else {
+      //update local state
+      const fileIndexDifference =
+        productImagesSource.length - imageFiles.length;
+      const indexToRemove = index - fileIndexDifference;
+      const newImageFiles = imageFiles.filter((_, i) => {
+        if (i !== indexToRemove) return true;
+      });
+      setImageFiles([...newImageFiles]);
+    }
+
+    //update ui
+    const newImagesSource = productImagesSource.filter((_, i) => {
+      if (i !== index) return true;
+    });
+    setProductImagesSource([...newImagesSource]);
   };
 
   const getBtnTitle = () => {
@@ -108,7 +130,7 @@ export default function ProductFormPage({
     if (files) {
       const newImages = Array.from(files).map((item) => item);
       const oldImages = productImagesSource || [];
-      setImages([...images, ...newImages]);
+      setImageFiles([...imageFiles, ...newImages]);
       setProductImagesSource([
         ...oldImages,
         ...newImages.map((file) => URL.createObjectURL(file)),
@@ -134,7 +156,7 @@ export default function ProductFormPage({
       <form
         action={() =>
           startTransition(async () => {
-            await onSubmit({ ...productInfo, images, thumbnail });
+            await onSubmit({ ...productInfo, images: imageFiles, thumbnail });
           })
         }
         className="space-y-6"
