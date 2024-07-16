a43b0021bf5721594be8acf06050198ff9190990
Ali Alipoor <alialipoordev@gmail.com>
2024-07-16 22:00:07 +0330

    cart model

---
+++ models/cartModel.ts
@@ -0,0 +1,39 @@
+import { Model, model, Models, models, ObjectId, Schema } from "mongoose";
+
+interface CartItem {
+  productId: ObjectId;
+  quantity: Number;
+}
+
+interface CartDocument extends Document {
+  userId: ObjectId;
+  items: CartItem[];
+}
+
+const cartSchema = new Schema<CartDocument>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const CartModel = models.Cart || model("Cart", cartSchema);
+
+export default CartModel as Model<CartDocument>;
