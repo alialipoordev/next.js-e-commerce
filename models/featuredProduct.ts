38b918f31d3fda246a18dacde483777863399f55
Ali Alipoor <alialipoordev@gmail.com>
2024-07-17 22:31:16 +0330

    featured product model

---
+++ models/featuredProduct.ts
@@ -0,0 +1,20 @@
+import { Model, model, models, Schema } from "mongoose";
+
+interface FeaturedProductDocument extends Document {
+  banner: { url: string; id: string };
+  title: string;
+  link: string;
+  linkTitle: string;
+}
+
+const featuredProductSchema = new Schema<FeaturedProductDocument>({
+  banner: { type: String, required: true },
+  title: { type: String, required: true },
+  link: { type: String, required: true },
+  linkTitle: { type: String, required: true },
+});
+
+const FeaturedProductModel =
+  models.FeaturedProduct || model("FeaturedProduct", featuredProductSchema);
+
+export default FeaturedProductModel as Model<FeaturedProductDocument>;
