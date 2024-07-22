728c62ec06040218bfcbd237697f17d4ce62c984
Ali Alipoor <alialipoordev@gmail.com>
2024-07-22 20:52:42 +0330

    fetch featured products

--- app/admin/products/featured/add/page.tsx
+++ app/admin/products/featured/add/page.tsx
@@ -1,12 +1,31 @@
 import FeaturedProductForm from "@/components/module/FeaturedProductForm";
 import FeaturedProductTable from "@/components/module/FeaturedProductTable";
+import connectDB from "@/lib/connectDB";
+import FeaturedProductModel from "@/models/featuredProduct";
 import React from "react";
 
-function AddFeaturedProduct() {
+const fetchFeaturedProducts = async () => {
+  await connectDB();
+  const products = await FeaturedProductModel.find();
+
+  return products.map(({ _id, title, link, linkTitle, banner }) => {
+    return {
+      id: _id.toString(),
+      title,
+      linkTitle,
+      link,
+      banner: banner.url,
+    };
+  });
+};
+
+async function AddFeaturedProduct() {
+  const featuredProducts = await fetchFeaturedProducts();
+
   return (
     <div>
       <FeaturedProductForm />
-      <FeaturedProductTable products={[]} />
+      <FeaturedProductTable products={featuredProducts} />
     </div>
   );
 }
