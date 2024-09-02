0462bc2e604d90752b51b7280f67bfcad8578b4d
Ali Alipoor <alialipoordev@gmail.com>
2024-09-02 12:51:55 +0330

    Review Model

---
+++ models/reviewModel.ts
@@ -0,0 +1,37 @@
+import { Document, Model, model, models, ObjectId, Schema } from "mongoose";
+
+interface ReviewDocument extends Document {
+  userId: ObjectId;
+  product: ObjectId;
+  rating: number;
+  comment?: string;
+  createdAt: Date;
+}
+
+const reviewSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const ReviewModel = models.Review || model("Review", reviewSchema);
+
+export default ReviewModel as Model<ReviewDocument>;
