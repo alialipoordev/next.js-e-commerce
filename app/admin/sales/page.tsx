dde09f0ca96094b355b66979269da4e0fd7eef7e
Ali Alipoor <alialipoordev@gmail.com>
2024-10-11 15:16:11 +0330

    fetch sales history

---
+++ app/admin/sales/page.tsx
@@ -0,0 +1,54 @@
+import OrderModel from "@/models/orderModel";
+import React from "react";
+import dateFormat from "dateformat";
+
+const sevenDaysSalesHistory = async () => {
+  const sevenDaysAgo = new Date();
+  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+  const dateList: string[] = [];
+  for (let i = 0; i < 7; i++) {
+    const date = new Date(sevenDaysAgo);
+    date.setDate(date.getDate() + i);
+    const dateString = date.toISOString().split("T")[0];
+    dateList.push(dateString);
+  }
+
+  const last7DaysSales: { _id: string; totalAmount: number }[] =
+    await OrderModel.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: sevenDaysAgo },
+          paymentStatus: "paid",
+        },
+      },
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+          totalAmount: { $sum: "$totalAmount" },
+        },
+      },
+    ]);
+
+  const sales = dateList.map((date) => {
+    const matchedSale = last7DaysSales.find((sale) => sale._id === date);
+    return {
+      day: dateFormat(date),
+      sale: matchedSale ? matchedSale.totalAmount : 0,
+    };
+  });
+
+  const totalSales = last7DaysSales.reduce((prevValue, { totalAmount }) => {
+    return (prevValue += totalAmount);
+  }, 0);
+
+  return { sales, totalSales };
+};
+
+async function Sales() {
+  const salesData = await sevenDaysSalesHistory();
+  
+  return <div>Sales</div>;
+}
+
+export default Sales;
