f5ebd8fd0437287ab7dee63304d3f009acd883a0
Ali Alipoor <alialipoordev@gmail.com>
2024-10-15 17:17:40 +0330

    history model

---
+++ models/historyModel.ts
@@ -0,0 +1,31 @@
+import { Document, Model, model, models, ObjectId, Schema } from "mongoose";
+
+interface HistoryDocument extends Document {
+  owner: ObjectId;
+  items: { product: ObjectId; date: Date }[];
+}
+
+const historySchema = new Schema<HistoryDocument>({
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  items: [
+    {
+      product: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+});
+
+const HistoryModel = models.History || model("History", historySchema);
+
+export default HistoryModel as Model<HistoryDocument>;
