17a278aaaace2c616024ba54a78f99270eac5732
Ali Alipoor <alialipoordev@gmail.com>
2024-10-22 16:01:11 +0330

    search filter UI

---
+++ app/(home_route)/search/page.tsx
@@ -0,0 +1,12 @@
+import SearchFilter from "@/components/module/SearchFilter";
+import React from "react";
+
+function Search() {
+  return (
+    <div>
+      <SearchFilter />
+    </div>
+  );
+}
+
+export default Search;
