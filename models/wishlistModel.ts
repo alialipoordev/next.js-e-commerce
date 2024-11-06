7bb5a557f70fd31ef7fd8aaf1187fb2a871472d7
Ali Alipoor <alialipoordev@gmail.com>
2024-11-06 17:13:51 +0330

    wishlist model

---
+++ models/wishlistModel.ts
@@ -0,0 +1,18 @@
+import { Document, Model, model, models, ObjectId, Schema } from "mongoose";
+
+interface WishlistDocument extends Document {
+  user: ObjectId;
+  products: ObjectId[];
+}
+
+const wishlistSchema = new Schema<WishlistDocument>(
+  {
+    user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    products: [{ type: Schema.Types.ObjectId, required: true, ref: "Product" }],
+  },
+  { timestamps: true }
+);
+
+const WishlistModel = models.Wishlist || model("Wishlist", wishlistSchema);
+
+export default WishlistModel as Model<WishlistDocument>;
